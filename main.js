d1d9581276ec875284a0b405dc32162891afb7bf
Matthew Neba <mattneba4343@gmail.com>
2024-08-18 02:41:48 -0600

    Initialize node project

---
+++ main.js
@@ -0,0 +1,3 @@
+// 1) Run the db client as an instance of the Event Emitter Class to be able to handle events when the network status changes
+// 2) Look into asyc/await and promises when sending commands to the db server, want to be able to send and then await the response
+// 3) Take another look at how node-redis works, will be similar to that
