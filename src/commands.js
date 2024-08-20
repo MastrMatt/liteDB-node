11d435f8f9ed778a9858e61397101d56ff8a5090
Matthew Neba <mattneba4343@gmail.com>
2024-08-20 03:39:03 -0600

    Start working on the command queue mechanism

---
+++ src/commands.js
@@ -0,0 +1,5 @@
+// the commands for the liteDB client
+
+function set(key, value) {
+    const cmdString = `set ${key} ${value}`;
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-// @ts-check
-
 // 1) Run the db client as an instance of the Event Emitter Class to be able to handle events when the network status changes
 // 2) Look into asyc/await and promises when sending commands to the db server, want to be able to send and then await the response
 // 3) Add pipeline support to the db client
 // 4) Take another look at how node-redis works, will be similar to that
+// 5) don't forgot to generate the .d.ts to support library users who use typescript
 // See how to store objects in the db, hashSet or a string as json.stringify?
 
 import { EventEmitter } from "events";
@@ -19,14 +18,20 @@ import {
 } from "./protocol.js";
 
 /**
-@typedef {Object} ClientOptions
-@property {string} host
-@property {number} port
+	@typedef {Object} ClientOptions
+	@property {string} host
+	@property {number} port
 */
 
 /**
+ * @typedef {Object} RecvData
+ * @property {number} type
+ * @property {number | string | Array<any>} [value]
+ */
+
+/**
  * Creates a new instance of the liteDB client
- * @param {ClientOptions} options
+ * @param {ClientOptions} [options] - The options to use when creating the client
  * @returns {liteDBClient}
  */
 function createClient(options) {
@@ -35,22 +40,20 @@ function createClient(options) {
 
 class liteDBClient extends EventEmitter {
 	/**
-	 * Creates a new instance of the liteDB client
-	 * @param {ClientOptions} options
+	 * @param {ClientOptions} [options]
 	 * @constructor
-	 *
-	 * 	*/
+	 */
 	constructor(options) {
 		super();
-		this.host = options.host || DEFAULT_SERVERIP;
-		this.port = options.port || DEFAULT_SERVERPORT;
+		this.host = options?.host || DEFAULT_SERVERIP;
+		this.port = options?.port || DEFAULT_SERVERPORT;
 		this.socket = new Socket();
 
 		// pipeline queue
 		this.commandQueue = [];
 	}
 
-	connect() {
+	async connect() {
 		return new Promise((resolve, reject) => {
 			this.socket.connect(this.port, this.host, () => {
 				this.emit("connect");
@@ -66,25 +69,21 @@ class liteDBClient extends EventEmitter {
 				this.emit("error", err);
 				reject(err);
 			});
-
-			// this should be attached and detached by the db command functions
-			// this.socket.on("data", (data) => {
-			// 	//  come back to this later, call some
-			// });
 		});
 	}
 
 	/**
 	 * Creates a new Buffer from the provided data, ensuring it does not exceed the specified maximum size.
 	 *
-	 * @param {string} data
 	 * @param {number} maxSize
+	 * @param {string} data
+	 *
 	 * @returns {Buffer} A Buffer containing the data
 	 */
-	createBuffer(data, maxSize) {
+	createBuffer(maxSize, data) {
 		if (Buffer.byteLength(data, "utf-8") > maxSize) {
 			// throw an error if the data exceeds the maximum size
-			throw new Error("Data exceeds the maximum size");
+			this.emit("error", new Error("Data exceeds maximum size"));
 		}
 		return Buffer.from(data);
 	}
@@ -93,12 +92,39 @@ class liteDBClient extends EventEmitter {
 	 * Sends a command to the server
 	 * @param {number} cmdLength
 	 * @param {string} cmdString
-	 * @returns {Promise}
 	 */
-	sendFormattedCmd(cmdLength, cmdString) {
-		// create a buffer to hold the command
-		let cmdBuffer = Buffer.alloc(MAX_MESSAGE_SIZE);
+	sendCmd(cmdLength, cmdString) {
+		// check if the command length exceeds the maximum allowed
+		if (cmdLength > MAX_MESSAGE_SIZE) {
+			this.emit(
+				"error",
+				new Error(
+					"Command length exceeds maximum string message size for server"
+				)
+			);
+		}
 
-		// write the command length to the buffer
+		// create a buffer to hold the length of the command
+		const cmdLengthBuffer = Buffer.alloc(4);
+		cmdLengthBuffer.writeInt32LE(cmdLength);
+
+		// create a buffer to hold the command string
+		const cmdBuffer = this.createBuffer(cmdLength, cmdString);
+
+		// concat
+		const cmd = Buffer.concat([cmdLengthBuffer, cmdBuffer], 4 + cmdLength);
+
+		// send the command to the server
+		this.socket.write(cmd);
 	}
 }
+
+// test the client
+const client = await createClient()
+	.on("error", (err) => {
+		console.error("Connection error occured");
+	})
+	.connect();
+
+client.sendCmd(4, "PING");
+client.sendCmd(6, "PINGER");
